b18c6ad14b380b4682a17901ffc46f89f4495850
scubasteve22 <sjwyer@googlemail.com>
2014-07-10 22:20:31 +1000

    Plot1 - Code and PNG
    Added the code to complete the first plot required

---
+++ plot1.R
@@ -0,0 +1,43 @@
+# Code to complete the first peer assignment - plot1 - 10/07/14
+
+#Set the location of the data to use
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+#Set the name of the zip file we wish to extract
+myzip = "exdata_data_household_power_consumption.zip"
+
+#If data file has not yet been downloaded then fetch it otherwise move on
+if (!file.exists(myzip)) {
+    download.file(fileurl, destfile=myzip,method="curl")
+    unzip(myzip)
+       
+    }
+
+#Read the unzipped text file into a set here
+housedata <- read.table("household_power_consumption.txt", sep=";", dec=".", header = TRUE)
+
+#Only keep what we need (was going to leverage SQL add on but no time)
+housedatakeep <- subset(housedata, housedata$Date == "1/2/2007" | housedata$Date == "2/2/2007")
+
+#Added a new column to create a datetime field not necessary for the specific plot but useful later
+housedatakeep$NewCol <- do.call(paste, c(housedatakeep[c("Date", "Time")], sep = " "))
+
+#Formatting the frame to ensure correct types for plotting
+houseformat<-data.frame(DateTime=strptime(housedatakeep$NewCol, format="%d/%m/%Y %H:%M:%S"), 
+                        Active=as.numeric(housedatakeep$Global_active_power),
+                        Reactive=as.numeric(as.character(housedatakeep$Global_reactive_power, dec=".")),
+                        Voltage=as.numeric(as.character(housedatakeep$Voltage, dec=".")),
+                        Intensity=as.numeric(as.character(housedatakeep$Global_intensity, dec=".")),
+                        Sub1=as.numeric(as.character(housedatakeep$Sub_metering_1, dec=".")),
+                        Sub2=as.numeric(as.character(housedatakeep$Sub_metering_2, dec=".")),
+                        Sub3=as.numeric(as.character(housedatakeep$Sub_metering_3, dec="."))
+                        )
+
+#Establish the file name and specific size for .png format
+png(filename = "plot1.png", width = 480, height = 480)
+
+##Create the histogram, set the colour and labels
+hist(houseformat$Active, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+#Close off process
+dev.off()
